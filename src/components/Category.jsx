c7d7faec6d39f99c20143c799cc01db7d15dd924
dev01yusuf <taoufikallahy@gmail.com>
2022-07-28 18:03:43 +0100

    adding navlink in category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,27 +1,27 @@
 import { FaPizzaSlice, FaHamburger} from "react-icons/fa";
 import { GiNoodles, GiChopsticks} from "react-icons/gi";
 import styled from "styled-components";
-/* import {NavLink} from 'react-router-dom'
- */
+import {NavLink} from 'react-router-dom'
+
 function Category() {
   return (
     <List>
-        <div>
+        <NavLink to={"/cuisine/Italian"}>
             <FaPizzaSlice />
             <h4>Italian</h4>
-        </div>
-        <div>
+        </NavLink>
+        <NavLink  to={"/cuisine/American"}>
             <FaHamburger />
             <h4>American</h4>
-        </div>
-        <div>
+        </NavLink>
+        <NavLink  to={"/cuisine/Thai"}>
             <GiNoodles />
-            <h4>Tahi</h4>
-        </div>
-        <div>
+            <h4>Thai</h4>
+        </NavLink>
+        <NavLink  to={"/cuisine/Japanese"}>
             <GiChopsticks />
             <h4>Japanese</h4>
-        </div>
+        </NavLink>
     </List>
   )
 }
